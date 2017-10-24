4c968f664d8b3108af0c35a72fa423d34b5013fa
Max Krebs <maxkrebs1@gmail.com>
2017-10-24 14:06:33 +0200

    Plot dynamisch 1

---
+++ content/python/plot_dyn80s.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.constants as const
+
+t, T1,T2,T3,T4,T5,T6,T7,T8 = np.genfromtxt('../values/messungdyn80s_val.txt',unpack=True)
+
+t /= 2
+T1 = const.convert_temperature(T1,'c','K')
+T2 = const.convert_temperature(T2,'c','K')
+T5 = const.convert_temperature(T5,'c','K')
+T6 = const.convert_temperature(T6,'c','K')
+
+
+plt.subplot(1, 2, 1)
+plt.plot(t, T2, 'r-', label="Messing anfang")
+plt.plot(t, T1, 'b-', label="Messing ende")
+plt.xlabel('t/s')
+plt.ylabel('T/K')
+plt.legend(loc='best')
+
+plt.subplot(1, 2, 2)
+plt.plot(t, T6, 'r-', label="Aluminium anfang")
+plt.plot(t, T5, 'b-', label="Aluminium ende")
+plt.xlabel('t/s')
+plt.ylabel('T/K')
+plt.legend(loc="best")
+plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+# in matplotlibrc leider (noch) nicht möglich
+plt.show()
+# plt.savefig('build/plot.pdf')
